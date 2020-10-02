09a082d97b186f98deb949154cab47b8aa4b3d8d
Anna Revidovich <nutkatuz@yandex.ru>
2020-10-02 18:29:15 +0500

    Updates

---
+++ precache-manifest.e06e30e164d11a4999eafb2f64734982.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89caff22cafd66b6d41c44aca20b7947",
+    "url": "/Hotel-Booking-Freebie/index.html"
+  },
+  {
+    "revision": "8bbbee9cc06e1c682efd",
+    "url": "/Hotel-Booking-Freebie/static/css/main.a7c47317.chunk.css"
+  },
+  {
+    "revision": "c2283b4ab7d148c45bfe",
+    "url": "/Hotel-Booking-Freebie/static/js/2.d5ff03fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hotel-Booking-Freebie/static/js/2.d5ff03fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbbee9cc06e1c682efd",
+    "url": "/Hotel-Booking-Freebie/static/js/main.4630094f.chunk.js"
+  },
+  {
+    "revision": "b3ac54af605ebd364a96",
+    "url": "/Hotel-Booking-Freebie/static/js/runtime-main.1b559e78.js"
+  },
+  {
+    "revision": "86aeb227a2eb1defe2a6fadab118f886",
+    "url": "/Hotel-Booking-Freebie/static/media/11814.86aeb227.ttf"
+  },
+  {
+    "revision": "e128ac44faa84b2d59c10c016fad0778",
+    "url": "/Hotel-Booking-Freebie/static/media/Mulish-Regular.e128ac44.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hotel-Booking-Freebie/static/media/logo.5d5d9eef.svg"
+  }
+]);
