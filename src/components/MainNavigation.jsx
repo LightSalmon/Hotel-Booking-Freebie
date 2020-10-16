9e468410a7cdc000d4148c131442804670ebe5fc
Tatiana Eskina <horechek444@gmail.com>
2020-10-16 20:24:25 +0300

    feat: окончание верстки header

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MainNavigation = () => {
+    return (
+        <div className="main-navigation">
+            <ul className="main-navigation__list">
+                <li className="main-navigation__item">All Deals</li>
+                <li className="main-navigation__item">Hotels</li>
+                <li className="main-navigation__item">Activities</li>
+                <li className="main-navigation__item">Hotel Day Packages</li>
+                <li className="main-navigation__item">Restaurants</li>
+                <li className="main-navigation__item">Events</li>
+                <li className="main-navigation__item">Rodrigues</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MainNavigation;
---
+++ src/components/TopNavigation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const TopNavigation = () => {
+    return (
+        <div className="header__top-navigation">
+            <div className="header__contact">
+                <span className="header__contact header__contact_type_question">Questions? Need Help?
+                </span> Call us on&nbsp;
+                <a href="tel:(+123) 456 1500" className="header__contact header__contact_type_phone"
+                   target="__blank">(+123) 456 1500</a>
+            </div>
+            <div className="header__preference">
+                <div className="header__container header__container_type_gift">
+                    <button className="header__button header__button_type_gift"/>
+                    <span className="header__program">BTP Gift Card</span>
+                </div>
+                <div className="header__container header__container_type_loyalty">
+                    <button className="header__button header__button_type_star"/>
+                    <span className="header__program">Loyalty Program</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TopNavigation;
